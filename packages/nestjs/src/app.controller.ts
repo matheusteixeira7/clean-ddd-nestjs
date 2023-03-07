b27f51fa218ca803b9e0bdc20d7560aa4f951fcb
Matheus Teixeira <matheus.teixeira7@gmail.com>
2023-03-06 21:42:33 -0300

    fix: imports in core package.json

--- packages/nestjs/src/app.controller.ts
+++ packages/nestjs/src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Product } from '@clean-ddd-nestjs/core/modules/product/domain';
 
 @Controller()
 export class AppController {
@@ -8,7 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log(Product);
     return this.appService.getHello();
   }
 }
